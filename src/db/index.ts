879ebc5723982099d059d9fbe00a0603b46c4d9a
Michael Chenevey <mchenevey7@gmail.com>
2025-01-24 18:20:42 -0800

    Create folder path if DNE

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,6 +4,15 @@ import { DatabaseSchema } from './schema'
 import { migrationProvider } from './migrations'
 
 export const createDb = (location: string): Database => {
+  if (location !== ':memory:') {
+    // make the directory if doesn't exist
+    const fs = require('fs')
+    const path = require('path')
+    const dir = path.dirname(location)
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true })
+    }
+  }
   return new Kysely<DatabaseSchema>({
     dialect: new SqliteDialect({
       database: new SqliteDb(location),
